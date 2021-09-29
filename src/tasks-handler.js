837deda98a010bfefb51ebb8275dfefc77f73114
Luis <lfmnovaes@gmail.com>
2021-09-29 18:32:44 -0300

    Fix testArray usage
    * Giving the same array for each test
    * Fix lint errors

--- src/tasks-handler.js
+++ src/tasks-handler.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable radix */
 class Crud {
-  // Method to add a new task to tasklist.
   static addTask(list, input) {
     if (input !== '') {
       const task = {
@@ -11,31 +8,30 @@ class Crud {
       };
       list.push(task);
       return 1;
-    } else {
-      return 0;
     }
+    return 0;
   }
 
-  // Method to remove a task from tasklist.
   static removeTask(list, id) {
-    if (id >= 0) {
-      const lastitem = list.length - 1;
-      if (id === lastitem) {
-        list.splice(id, 1);
-      } else {
-        list.splice(id, 1);
-        let j = parseInt(id);
-        for (j; j < list.length; j++) {
-          list[j].index -= 1;
+    if (list.length > 0) {
+      if (id >= 0) {
+        const lastitem = list.length - 1;
+        if (id === lastitem) {
+          list.splice(id, 1);
+        } else {
+          list.splice(id, 1);
+          let j = parseInt(id, 10);
+          for (j; j < list.length; j += 1) {
+            list[j].index -= 1;
+          }
         }
+        return 1;
       }
-      return 1;
-    } else {
       return 0;
     }
+    return 0;
   }
 
-  // Method to update task description.
   static updateDescription(list, index, input) {
     list[index].description = input;
     return list[index].description;
--- src/tasks.handler.test.js
+++ src/tasks.handler.test.js
@@ -1,36 +1,36 @@
-// const addTask = require('./tasks-handler');
 const crud = require('./tasks-handler');
 
-const testArray = [];
-let testArray2 = [{
-  description: 'blabla',
+const testArray = () => [];
+
+const testArray2 = () => [{
+  description: 'Lorem Ipsum',
   completed: false,
   index: 0,
 }];
 
-
-
-
-describe('Updating tasks', () => {
-  test('compare the same string is inserted', () => {
-    expect( crud.updateDescription(testArray2, 0, 'Alex')==='Alex' ).toBe(true);
-  });
-}); 
-
 describe('Adding new tasks', () => {
   test('Adding Hi Task', () => {
-    expect(crud.addTask(testArray, 'Hi')).toBe(1);
+    expect(crud.addTask(testArray(), 'Hi')).toBe(1);
   });
   test('Adding an empty new task', () => {
-    expect(crud.addTask(testArray, '')).toBe(0);
+    expect(crud.addTask(testArray(), '')).toBe(0);
   });
 });
 
 describe('Removing tasks', () => {
+  test('Remove one task on an empty array', () => {
+    expect(crud.removeTask(testArray(), '0')).toBe(0);
+  });
   test('Remove one task with -1 id', () => {
-    expect(crud.removeTask(testArray2, '-1')).toBe(0);
+    expect(crud.removeTask(testArray2(), '-1')).toBe(0);
   });
   test('Remove one task', () => {
-    expect(crud.removeTask(testArray2, '0')).toBe(1);
+    expect(crud.removeTask(testArray2(), '0')).toBe(1);
   });
-}); 
+});
+
+describe('Updating tasks', () => {
+  test('compare the same string is inserted', () => {
+    expect(crud.updateDescription(testArray2(), 0, 'Alex') === 'Alex').toBe(true);
+  });
+});
